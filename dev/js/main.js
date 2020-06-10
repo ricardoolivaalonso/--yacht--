2095992f69c1a8dea6b960c8adceb3d8419639ff
Ricardo Oliva Alonso <r.oliva.alonso@gmail.com>
2020-06-10 09:29:42 -0500

    ADD => Mobile filter

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,6 +1,17 @@
-let pEl = document.getElementsByTagName('button')
+/*===== Variables =====*/
+let fmTrigger = document.querySelector('#fm-trigger')
+let fmPrice = document.querySelector('#fm-price')
+let modal = document.querySelector('#modal')
 
-let preventFunc = (e) => e.preventDefault()
+/*===== Funciones =====*/
 
-for (var i = 0; i < pEl.length; i++) 
-    pEl[i].addEventListener('click', preventFunc)
+let showPrice = () => {
+    fmPrice.classList.toggle('is-price-visible')
+    modal.classList.toggle('is-modal-visible')
+}
+
+/*===== Listeners =====*/
+
+
+fmTrigger.addEventListener('click', showPrice)
+modal.addEventListener('click', showPrice)
--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,17 @@
 "use strict";
 
-var pEl = document.getElementsByTagName('button');
+/*===== Variables =====*/
+var fmTrigger = document.querySelector('#fm-trigger');
+var fmPrice = document.querySelector('#fm-price');
+var modal = document.querySelector('#modal');
+/*===== Funciones =====*/
 
-var preventFunc = function preventFunc(e) {
-  return e.preventDefault();
+var showPrice = function showPrice() {
+  fmPrice.classList.toggle('is-price-visible');
+  modal.classList.toggle('is-modal-visible');
 };
+/*===== Listeners =====*/
 
-for (var i = 0; i < pEl.length; i++) {
-  pEl[i].addEventListener('click', preventFunc);
-}
+
+fmTrigger.addEventListener('click', showPrice);
+modal.addEventListener('click', showPrice);
