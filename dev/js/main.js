930df42da59743560a526d79e005c8c2b26480bb
Ricardo Oliva Alonso <r.oliva.alonso@gmail.com>
2020-06-10 13:54:45 -0500

    Update => New cards & Menu

--- dev/js/main.js
+++ dev/js/main.js
@@ -2,16 +2,16 @@
 let fmTrigger = document.querySelector('#fm-trigger')
 let fmPrice = document.querySelector('#fm-price')
 let modal = document.querySelector('#modal')
+let filterM = document.querySelectorAll('.filter-mobile__range .filter-form__label')
+let filterR = document.querySelector('#fm-r')
 
 /*===== Funciones =====*/
-
 let showPrice = () => {
     fmPrice.classList.toggle('is-price-visible')
     modal.classList.toggle('is-modal-visible')
 }
-
+let showFilter = () => filterR.classList.toggle('is-filter-tx')
 /*===== Listeners =====*/
-
-
 fmTrigger.addEventListener('click', showPrice)
 modal.addEventListener('click', showPrice)
+filterM.forEach( f => f.addEventListener('click', showFilter) );
--- public/js/main.js
+++ public/js/main.js
@@ -4,14 +4,23 @@
 var fmTrigger = document.querySelector('#fm-trigger');
 var fmPrice = document.querySelector('#fm-price');
 var modal = document.querySelector('#modal');
+var filterM = document.querySelectorAll('.filter-mobile__range .filter-form__label');
+var filterR = document.querySelector('#fm-r');
 /*===== Funciones =====*/
 
 var showPrice = function showPrice() {
   fmPrice.classList.toggle('is-price-visible');
   modal.classList.toggle('is-modal-visible');
 };
+
+var showFilter = function showFilter() {
+  return filterR.classList.toggle('is-filter-tx');
+};
 /*===== Listeners =====*/
 
 
 fmTrigger.addEventListener('click', showPrice);
 modal.addEventListener('click', showPrice);
+filterM.forEach(function (f) {
+  return f.addEventListener('click', showFilter);
+});
